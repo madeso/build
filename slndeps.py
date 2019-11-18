d6e7c01cb406f6498d13c31f5965c791ade5a83f
Gustav <sir.gustav.the.coder@gmail.com>
2019-11-18 15:27:53 +0100

    updaded typehints to modern python

--- slndeps.py
+++ slndeps.py
@@ -1,10 +1,14 @@
+#!/usr/bin/env python3
+
 import argparse
 import os
 import os.path
 import xml.etree.ElementTree as ET
+from enum import Enum
 import re
 import os
 import subprocess
+import typing
 
 
 # ======================================================================================================================
@@ -19,28 +23,11 @@ class Build(Enum):
 
 
 class Project:
-    """
-    :type Solution: Solution
-    :type DisplayName: string
-    :type Path: string
-    :type Id: string
-    :type Type: Build
-    :type deps: list[Project]
-    :type sdeps: list[string]
-    """
-
-    def __init__(self, Solution, Name, Path, Id):
-        """
-        :type Solution: Solution
-        :type Name: string
-        :type Path: string
-        :type Id: string
-        """
-
+    def __init__(self, Solution: 'Solution', Name: str, Path: str, Id: str):
         self.DisplayName = ""
         self.Type = Build.Unknown
-        self.deps = []
-        self.sdeps = []
+        self.deps: typing.List['Project'] = []
+        self.sdeps: typing.List[str] = []
 
         self.Solution = Solution
         self.Name = Name
@@ -48,26 +35,17 @@ class Project:
         self.Id = Id
 
     @property
-    def Name(self):
-        """
-        @retype: string
-        """
+    def Name(self) -> str:
         return self.DisplayName.replace('-', '_').replace('.', '_')
 
     @Name.setter
-    def Name(self, value):
-        """
-        :param value: string
-        """
+    def Name(self, value: str):
         self.DisplayName = value
 
     def __str__(self):
         return self.Name
 
-    def resolve(self, projects):
-        """
-        :param projects: dict[string, Project]
-        """
+    def resolve(self, projects: typing.Dict[str, 'Project']):
         # print("Resolving ", self.Name, len(self.sdeps))
         for ss in self.sdeps:
             s = ss.lower()
@@ -116,18 +94,12 @@ class Project:
             inner_text = n.text.strip()
             self.sdeps_append(inner_text)
 
-    def sdeps_append(self, dep):
-        """
-        :type dep: string
-        """
+    def sdeps_append(self, dep: str):
         self.sdeps.append(dep.lower())
         pass
 
-def Gen(pa):
-    """
-    :param pa: string
-    :return: string
-    """
+
+def Gen(pa: str) -> str:
     p = pa
     if os.path.isfile(p):
         return p
@@ -157,22 +129,11 @@ class Solution:
     """ @type projects: dict[string, Project] """
     """ @type includes: list[string] """
 
-    def __init__(self, slnpath, exclude, simplify, reverseArrows):
-        """
-        @type slnpath: string
-        @type exclude:  list[string]
-        @type simplify: bool
-        @type reverseArrows: bool
-        """
+    def __init__(self, slnpath: str, exclude: typing.List[str], simplify: bool, reverseArrows: bool):
         self.reverseArrows = reverseArrows
         self.setup(slnpath, exclude, simplify)
 
-    def setup(self, slnpath, exclude, dosimplify):
-        """
-        @type slnpath: string
-        @type exclude:  list[string]
-        @type dosimplify: bool
-        """
+    def setup(self, slnpath: str, exclude: typing.List[str], dosimplify: bool):
         self.includes = exclude
         self.Name = os.path.basename(os.path.splitext(slnpath)[0])
         with open(slnpath) as f:
@@ -186,7 +147,7 @@ class Solution:
                 name = data[0].strip().strip('"').strip()
                 relativepath = data[1].strip().strip('"').strip()
                 id = data[2].strip().strip('"').strip()
-                currentProject = project.Project(Solution=self, Name=name, Path=os.path.join(os.path.dirname(slnpath), relativepath), Id=id)
+                currentProject = Project(Solution=self, Name=name, Path=os.path.join(os.path.dirname(slnpath), relativepath), Id=id)
                 self.projects[currentProject.Id.lower()] = currentProject
             elif line == "EndProject":
                 currentProject = None
@@ -204,29 +165,24 @@ class Solution:
             p.resolve(self.projects)
 
     @property
-    def Graphviz(self):
-        """
-        @retype: list[string]
-        """
-        lines = []
-        """:type : list[string]"""
-        lines.append("digraph " + self.Name.replace("-", "_") + " {")
-        lines.append("/* projects */")
+    def Graphviz(self) -> typing.Iterable[str]:
+        yield "digraph " + self.Name.replace("-", "_") + " {"
+        yield "/* projects */"
         for _, pro in self.projects.items():
             if self.Exclude(pro.DisplayName):
                 continue
             decoration = "label=\"" + pro.DisplayName + "\""
             shape = "plaintext"
-            if pro.Type == project.Build.Application:
+            if pro.Type == Build.Application:
                 shape = "folder"
-            elif pro.Type == project.Build.Shared:
+            elif pro.Type == Build.Shared:
                 shape = "ellipse"
-            elif pro.Type == project.Build.Static:
+            elif pro.Type == Build.Static:
                 shape = "component"
             decoration += ", shape=" + shape
-            lines.append(" " + pro.Name + " [" + decoration + "]" + ";")
-        lines.append("")
-        lines.append("/* dependencies */")
+            yield " " + pro.Name + " [" + decoration + "]" + ";"
+        yield ""
+        yield "/* dependencies */"
         addspace = False
         for _, p in self.projects.items():
             if len(p.deps) == 0:
@@ -235,61 +191,42 @@ class Solution:
             if self.Exclude(p.Name):
                 continue
             if addspace:
-                lines.append("")
+                yield ""
             else:
                 addspace = True
             for s in p.deps:
                 if self.Exclude(s.DisplayName):
                     continue
                 if self.reverseArrows:
-                    lines.append(" " + s.Name + " -> " + p.Name + ";")
+                    yield " " + s.Name + " -> " + p.Name + ";"
                 else:
-                    lines.append(" " + p.Name + " -> " + s.Name + ";")
-        lines.append("}")
-        return lines
+                    yield " " + p.Name + " -> " + s.Name + ";"
+        yield "}"
 
-    def Exclude(self, p):
-        """
-        @type p: string
-        @retype: bool
-        """
+    def Exclude(self, p: str) -> bool:
         if self.includes is not None:
             for s in self.includes:
                 if s.lower().strip() == p.lower().strip():
                     return True
         return False
 
-    def writeGraphviz(self, targetFile):
-        """
-        @type targetFile: string
-        """
+    def writeGraphviz(self, targetFile: str):
         with open(targetFile, 'w') as f:
             for line in self.Graphviz:
                 f.write(line + "\n")
 
     def simplify(self):
-        """
-        """
         for _, pe in self.projects.items():
             self.ssimplify(pe)
 
-    def ssimplify(self, p):
-        """
-        @type p: Project
-        """
+    def ssimplify(self, p: Project):
         deps = []
         for d in p.sdeps:
             if not self.hasDependency(p, d, False):
                 deps.append(d)
         p.sdeps = deps
 
-    def hasDependency(self, p, sd, self_reference):
-        """
-        @type p: Project
-        @type sd: string
-        @type self_reference: bool
-        @retype: bool
-        """
+    def hasDependency(self, p: Project, sd: str, self_reference: bool) -> bool:
         for d in p.sdeps:
             if self_reference and d == sd:
                 return True
@@ -298,12 +235,7 @@ class Solution:
         return False
 
 
-def IsValidFile(lines):
-    """
-    static
-    @type lines: list[string]
-    @retype bool
-    """
+def IsValidFile(lines: typing.List[str]) -> bool:
     for line in lines:
         if line == "Microsoft Visual Studio Solution File, Format Version 10.00":
             return True
@@ -315,33 +247,7 @@ def IsValidFile(lines):
 # ======================================================================================================================
 
 
-def toGraphviz(source, target, format, exclude, simplify, style, reverseArrows):
-    """
-    public
-    :type source: string
-    :type target: string
-    :type format: string
-    :type exclude: list[string]
-    :type: simplify: bool
-    :type style: string
-    :type reverseArrows: bool
-    """
-    my_target = target or ""
-    if my_target.strip() == "" or my_target.strip() == "?":
-        my_target = os.path.splitext(source)[0]
-    my_format = format or ""
-    if my_format.strip() == "" or my_format.strip() == "?":
-        my_format = "svg"
-    logic(source, exclude or [], my_target, my_format, simplify, style or "dot", reverseArrows)
-
-def getFile(source, target, format):
-    """
-    internal
-    :rtype: string
-    :type source: string
-    :type target: string
-    :type format: string
-    """
+def getFile(source: str, target: str, format: str) -> str:
     my_target = target
     if my_target.strip() == "" or my_target.strip() == "?":
         my_target = os.path.splitext(source)[0]
@@ -353,41 +259,33 @@ def getFile(source, target, format):
     f = my_target + "." + my_format
     return f
 
-def GetProjects(source):
-    """
-    public
-    :rtype: IEnumerable<string>
-    :type source: string
-    """
-    s = solution.Solution(source, [], False, False)
+
+def GetProjects(source: str) -> typing.Iterable[str]:
+    s = Solution(source, [], False, False)
     for p in s.projects:
         yield p.Value.Name
 
-def logic(solutionFilePath, exlude, targetFile, format, simplify, style, reverseArrows):
-    """
-    private
-    :type solutionFilePath: string
-    :type exlude: List<string>
-    :type targetFile: string
-    :type format: string
-    :type simplify: bool
-    :type style: string
-    :type reverseArrows: bool
-    """
-    s = solution.Solution(solutionFilePath, exlude, simplify, reverseArrows)
-    s.writeGraphviz(targetFile + ".graphviz")
-    graphviz(targetFile, format, style)
 
-def graphviz(targetFile, format, style):
-    """
-    private
-    :type targetFile: string
-    :type format: string
-    :type style: string
-    """
+def graphviz(targetFile: str, format: str, style: str):
     cmdline = ["dot", targetFile+".graphviz", "-T" + format, "-K" + style, "-O" + targetFile + "." + format]
     print("Running graphviz ", cmdline)
     s = subprocess.call(cmdline)
+
+
+def logic(solutionFilePath: str, exlude: typing.List[str], targetFile: str, format: str, simplify: bool, style: str, reverseArrows: bool):
+    s = Solution(solutionFilePath, exlude, simplify, reverseArrows)
+    s.writeGraphviz(targetFile + ".graphviz")
+    graphviz(targetFile, format, style)
+
+
+def toGraphviz(source: str, target: str, format: str, exclude: typing.List[str], simplify: bool, style: bool, reverseArrows: bool):
+    my_target = target or ""
+    if my_target.strip() == "" or my_target.strip() == "?":
+        my_target = os.path.splitext(source)[0]
+    my_format = format or ""
+    if my_format.strip() == "" or my_format.strip() == "?":
+        my_format = "svg"
+    logic(source, exclude or [], my_target, my_format, simplify, style or "dot", reverseArrows)
 
 
 # ======================================================================================================================
@@ -407,122 +305,4 @@ if __name__ == "__main__":
     parser.add_argument('--reverse', dest='reverse', action='store_const', const=True, default=False, help='reverse arrows')
 
     args = parser.parse_args()
-    logic.toGraphviz(args.solution, args.target, args.format, args.exclude, args.simplify, args.style, args.reverse)
-# using System;
-# using System.Collections.Generic;
-# using System.ComponentModel;
-# using System.Data;
-# using System.Drawing;
-# using System.Linq;
-# using System.Text;
-# using System.Windows.Forms;
-# using System.IO;
-# using System.Xml;
-#
-# namespace SlnDeps
-# {
-# 	public partial class Main : Form
-# 	{
-# 		public Main()
-# 		{
-# 			InitializeComponent();
-# 		}
-#
-# 		private void dBrowseSource_Click(object sender, EventArgs e)
-# 		{
-# 			if (ofd.ShowDialog() != DialogResult.OK) return;
-# 			dSource.Text = ofd.FileName;
-# 		}
-#
-# 		private void dBrowseTarget_Click(object sender, EventArgs e)
-# 		{
-# 			if (sfd.ShowDialog() != DialogResult.OK) return;
-# 			dTarget.Text = sfd.FileName;
-# 		}
-#
-# 		private void dGo_Click(object sender, EventArgs e)
-# 		{
-# 			saveSettings();
-# 			List<string> items = new List<string>();
-# 			foreach (var c in dExcludes.CheckedItems)
-# 			{
-# 				items.Add(c.ToString());
-# 			}
-# 			if (items.Count == 0) items = null;
-# 			Logic.toGraphviz(dSource.Text, dTarget.Text, dFormat.Text, items, dSimplifyLinks.Checked, dStyle.Text, dReverseArrows.Checked);
-# 		}
-#
-# 		private void dFillExcludes_Click(object sender, EventArgs e)
-# 		{
-# 			if (dExcludes.Items.Count != 0)
-# 			{
-# 				if (MessageBox.Show("Items is not empty, continue?", "Continue?", MessageBoxButtons.YesNo) != DialogResult.Yes) return;
-# 			}
-#
-# 			dExcludes.Items.Clear();
-# 			Dictionary<string, string> settings = LoadSettings(ConfigurationFile);
-#
-# 			foreach (var s in Logic.GetProjects(dSource.Text))
-# 			{
-# 				dExcludes.Items.Add(s, GetBool(settings, s, true));
-# 			}
-# 		}
-#
-# 		void saveSettings()
-# 		{
-# 			Dictionary<string, string> settings = new Dictionary<string, string>();
-# 			for (int i = 0; i < dExcludes.Items.Count; ++i )
-# 			{
-# 				bool ch = dExcludes.CheckedIndices.Contains(i);
-# 				string name = dExcludes.Items[i].ToString();
-# 				settings[name] = ch.ToString();
-# 			}
-#
-# 			SaveSettings(ConfigurationFile, settings);
-# 		}
-#
-# 		private string ConfigurationFile
-# 		{
-# 			get
-# 			{
-# 				return Path.ChangeExtension(dSource.Text, "slndeps");
-# 			}
-# 		}
-#
-# 		bool GetBool(Dictionary<string, string> dict, string name, bool def)
-# 		{
-# 			if (dict.ContainsKey(name))
-# 			{
-# 				return bool.Parse(dict[name]);
-# 			}
-# 			else return def;
-# 		}
-#
-# 		private Dictionary<string, string> LoadSettings(string file)
-# 		{
-# 			Dictionary<string, string> dict = new Dictionary<string,string>();
-# 			if (false == File.Exists(file)) return dict;
-# 			var lines = File.ReadAllLines(file);
-# 			foreach (var ol in lines)
-# 			{
-# 				var l = ol.Trim();
-# 				if (l.StartsWith("#")) continue;
-# 				var sp = l.Split("=".ToCharArray(), 2);
-# 				if (sp.Length != 2) continue;
-# 				var k = sp[0].Trim();
-# 				var v = sp[1].Trim();
-# 				dict[k] = v;
-# 			}
-#
-# 			return dict;
-# 		}
-#
-# 		private void SaveSettings(string file, Dictionary<string, string> dict)
-# 		{
-# 			List<string> lines = new List<string>();
-# 			foreach (var e in dict)
-# 			{
-# 				lines.Add( string.Format("{0}={1}", e.Key, e.Value) );
-# 			}
-# 			File.WriteAllLines(file, lines.ToArray());
-# 		}
+    toGraphviz(args.solution, args.target, args.format, args.exclude, args.simplify, args.style, args.reverse)
