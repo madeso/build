de4b28504a012d965c0e752d8460b77db38ab2b5
Gustav <sir.gustav.the.coder@gmail.com>
2021-12-22 11:19:18 +0100

    feat: better header

--- src/printer.rs
+++ src/printer.rs
@@ -16,12 +16,36 @@ impl Printer
         }
     }
 
-    pub fn header(&self, text: &str)
+    // print a "pretty" header to the terminal
+    pub fn header(&self, project_name: &str)
     {
-        println!("");
-        println!("############################################");
-        println!("##  {}", text);
-        println!("############################################");
+        self.header_with_custom_char(project_name, "-");
+    }
+
+    pub fn header_with_custom_char(&self, project_name: &str, header_character: &str)
+    {
+        // todo(Gustav): replace len with https://stackoverflow.com/a/46290728 if needed
+        // or with https://users.rust-lang.org/t/fill-string-with-repeated-character/1121/9
+
+        let header_size = 65;
+        let header_spacing = 1;
+        let header_start = 3;
+
+        let spacing_string = " ".repeat(header_spacing);
+        let header_string = header_character.repeat(header_size);
+
+        let project = format!("{}{}{}", spacing_string, project_name, spacing_string);
+        let start = header_character.repeat(header_start);
+
+        let left = header_size - (project.len() + header_start);
+        let right =
+            if left > 1 { header_character.repeat(left) }
+            else {String::from("")}
+            ;
+        
+        println!("{}", header_string);
+        println!("{}{}{}", start, project, right);
+        println!("{}", header_string);
     }
 
     pub fn info(&self, text: &str)
