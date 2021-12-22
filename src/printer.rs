eebdcbfe8793e6a0aa5736b6b596fc940b34e11f
Gustav <sir.gustav.the.coder@gmail.com>
2021-12-22 12:13:32 +0100

    feat: print file

--- src/printer.rs
+++ src/printer.rs
@@ -1,9 +1,13 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+
+
 pub struct Printer
 {
     error_count: i32,
     errors: Vec<String>
 }
-
 
 impl Printer
 {
@@ -60,6 +64,25 @@ impl Printer
         println!("ERROR: {}", text);
     }
 
+    pub fn file(&self, path: &str)
+    {
+        if let Ok(lines) = read_lines(path)
+        {
+            println!("{}>", path);
+            for line in lines
+            {
+                if let Ok(ip) = line
+                {
+                    println!("---->{}", ip);
+                }
+            }
+        }
+        else
+        {
+            println!("Failed to open '{}'", path);
+        }
+    }
+
     pub fn exit_with_code(&self)
     {
         if self.error_count > 0
@@ -74,3 +97,11 @@ impl Printer
     }
 }
 
+
+// The output is wrapped in a Result to allow matching on errors
+// Returns an Iterator to the Reader of the lines of the file.
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>> where P: AsRef<Path>,
+{
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
