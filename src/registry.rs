d17ab1aa9808a0d91823a35f560a6f19bfccb4de
Gustav <sir.gustav.the.coder@gmail.com>
2021-12-25 08:49:34 +0100

    feat: started implementing windows function

--- src/registry.rs
+++ src/registry.rs
@@ -1,10 +1,11 @@
 // access registry on windows, returns None on non-windows
 
-// #[cfg(target_os = "windows")]
-// pub fn hklm(key_name: &str, value_name& str) -> Result<String>
-// {
-//    use https://crates.io/crates/winreg
-// }
+#[cfg(target_os = "windows")]
+pub fn hklm(key_name: &str, value_name: &str) -> Result<String, String>
+{
+    Err(String::from("Not implemented."))
+    // use https://crates.io/crates/winreg
+}
 
 #[cfg(not(target_os = "windows"))]
 pub fn hklm(_: &str, _: & str) -> Result<String, String>
