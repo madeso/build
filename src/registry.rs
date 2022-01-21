65be1cc9aa4cf93cb6fa89c3d0807107aee0616e
Gustav <sir.gustav.the.coder@gmail.com>
2022-01-21 21:46:07 +0100

    fix: windows crate isn't available on non-windows

--- src/registry.rs
+++ src/registry.rs
@@ -1,14 +1,17 @@
 // access registry on windows, returns None on non-windows
 
+#[cfg(target_os = "windows")]
 extern crate winreg;
-use std::io;
-use std::path::Path;
-use winreg::enums::*;
-use winreg::RegKey;
+
 
 #[cfg(target_os = "windows")]
 pub fn hklm(key_name: &str, value_name: &str) -> io::Result<String>
 {
+    use winreg::enums::*;
+    use winreg::RegKey;
+    use std::io;
+    use std::path::Path;
+
     let hklm = RegKey::predef(HKEY_LOCAL_MACHINE);
     let cur_ver = hklm.open_subkey(key_name)?; // "SOFTWARE\\Microsoft\\Windows\\CurrentVersion"
     let value: String = cur_ver.get_value(value_name)?; // "ProgramFilesDir"
