36b11806e267a27dd5d64c0a8233be9cd2dedd54
Gustav <sir.gustav.the.coder@gmail.com>
2023-02-25 12:36:52 +0100

    Prefix with workbench if files are in root

--- Workbench/FileNames.cs
+++ Workbench/FileNames.cs
@@ -2,10 +2,12 @@ namespace Workbench;
 
 internal static class FileNames
 {
-    internal static readonly string BuildSettings = "settings.wb.jsonc";
-    internal static readonly string ClangTidyStore = "clang-tidy-store.wb.jsonc";
+    // this is stored in /build/ so its meaning is implied
+    internal static readonly string BuildSettings = "settings.jsonc";
 
-    internal static readonly string BuildData = "build.wb.jsonc";
-    internal static readonly string Includes = "includes.wb.jsonc";
-    internal static readonly string CheckNames = "names.wb.jsonc";
+    internal static readonly string ClangTidyStore = ".workbench.clang-tidy-store.jsonc";
+
+    internal static readonly string BuildData = "workbench.build.jsonc";
+    internal static readonly string Includes = "workbench.check-includes.jsonc";
+    internal static readonly string CheckNames = "workbench.check-names.jsonc";
 }
