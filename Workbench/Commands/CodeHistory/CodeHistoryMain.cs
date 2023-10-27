ccd13aa40475318a64f178dd55eeec59a00a66f8
Gustav <sir.gustav.the.coder@gmail.com>
2023-10-27 08:46:02 +0200

    Add option to display files in code history

--- Workbench/Commands/CodeHistory/CodeHistoryMain.cs
+++ Workbench/Commands/CodeHistory/CodeHistoryMain.cs
@@ -18,10 +18,16 @@ internal sealed class PrintCodeHistory : AsyncCommand<PrintCodeHistory.Arg>
     public sealed class Arg : CommandSettings
     {
         [CommandArgument(0, "<Resolution>")]
+        [Description("The resolution to group on")]
         public TimeResolution Resolution { get; set; } = TimeResolution.Month;
 
         [CommandOption("--all-files")]
+        [Description("Look at all files and not just source files")]
         public bool AllFiles { get; set; } = false;
+
+        [CommandOption("--show-files")]
+        [Description("Show the files that include the age count")]
+        public bool ShowFiles { get; set; } = false;
     }
 
     public override async Task<int> ExecuteAsync([NotNull] CommandContext context, [NotNull] Arg arg)
@@ -40,15 +46,18 @@ internal sealed class PrintCodeHistory : AsyncCommand<PrintCodeHistory.Arg>
             .MapArrayAsync(files.ToImmutableArray(), async file =>
             {
                 var blamed = await Shared.Git.BlameAsync(file)
-                    .SelectAsync(line => line.Author.Time)
+                    .SelectAsync(line => new {File=file, line.Author.Time})
                     .ToListAsync();
                 return ($"Blaming {file.GetRelative(cwd)}", blamed);
             }))
             .SelectMany(x => x);
 
         var grouped = blamed_times
-            .Order()
-            .GroupOnTime(x => x, arg.Resolution, (title, times) => new {Title=title.ToString(arg.Resolution), Count=times.Count})
+            .OrderBy(x=> x.Time)
+            .GroupOnTime(x => x.Time, arg.Resolution, (title, times) => new {
+                Title=title.ToString(arg.Resolution),
+                Files=times.Select(x => x.File).DistinctBy(x => x.FullName).ToImmutableArray(),
+                times.Count})
             .ToImmutableArray();
 
         // display histogram
@@ -58,6 +67,18 @@ internal sealed class PrintCodeHistory : AsyncCommand<PrintCodeHistory.Arg>
             .CenterLabel()
             .AddItems(grouped, item => new BarChartItem(
                 item.Title, item.Count, Color.Blue)));
+
+        if (!arg.ShowFiles) { return 0; }
+
+        foreach (var e in grouped)
+        {
+            Printer.Header($"{e.Title} ({e.Count})");
+            foreach (var f in e.Files)
+            {
+                AnsiConsole.WriteLine(f.GetRelative(cwd));
+            }
+            AnsiConsole.WriteLine();
+        }
 
         return 0;
     }
